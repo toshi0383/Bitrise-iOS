c0f2862aac86c0094ec2ae0be9fa686f29e47d38
toshi0383 <t.suzuki326@gmail.com>
2018-02-27 23:15:41 +0900

    Fix project.yml

--- BitriseClient/Sources/AppDelegate.swift
+++ BitriseClient/Sources/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        #if TEST
+        if ProcessInfo.processInfo.environment["TEST"] == "YES" {
             return true
-        #endif
+        }
 
         // ActionPopoverButton
         UIView.hth.exchangeMethods()
