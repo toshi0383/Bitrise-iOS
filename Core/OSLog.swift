d60e77a3ddb76f5d3c4b963a9eada70f29db7b32
toshi0383 <t.suzuki326@gmail.com>
2018-11-07 16:36:25 +0900

    opt-out signpost

--- Core/OSLog.swift
+++ Core/OSLog.swift
@@ -1,8 +1,17 @@
 import os.log
 
 extension OSLog {
-    public static let network = OSLog(subsystem: "jp.toshi0383.Bitrise-iOS.Core", category: "Network")
+    public static let network = {
+        ProcessInfo.processInfo.environment["ENABLE_SIGNPOST"] == "YES"
+            ? OSLog(subsystem: "jp.toshi0383.Bitrise-iOS.Core", category: "Network")
+            : .disabled
+    }()
 
     @available(iOS 12.0, *)
-    public static let pointsOfInterest = OSLog(subsystem: "jp.toshi0383.Bitrise-iOS", category: .pointsOfInterest)
+    public static let pointsOfInterest = {
+        ProcessInfo.processInfo.environment["ENABLE_SIGNPOST"] == "YES"
+            ? OSLog(subsystem: "jp.toshi0383.Bitrise-iOS", category: .pointsOfInterest)
+            : .disabled
+    }()
+
 }
